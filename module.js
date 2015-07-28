b30310e869bca6e8d328ffaf1a76f8d7d7454d0c
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-07-28 17:44:59 +0200

    Fix reference

--- module.js
+++ module.js
@@ -1,5 +1,5 @@
 import placeholder from './module/_';
 import curry_ from './module/curry';
 
-export const _ = placeholder(Symbol);
+export const _ = placeholder({Symbol});
 export const curry = curry_({_});
