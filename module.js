44a186a81b1234f852ec862a2bfc048063da7f67
Bob Myers <rtm@gol.com>
2015-10-22 14:27:01 +0530

    Change placeholder to `{placeholder: 'curry-this'}

--- module.js
+++ module.js
@@ -1,4 +1,9 @@
 import curryFunction from './module/curry';
 
-export const _ = {};
+// The placeholder could be any uniquely identifiable object.
+// Its contents are irrelevant.
+// We provide it with a `placeholder` property only to make it easier to
+// see what it is in a debugger window, for example.
+export const _ = {placeholder: 'curry-this'};
+
 export const curry = curryFunction({placeholder: _});
